21dc9080c37556cc602bf9ee8b0d2c834ec26fa1
kevinclelland <kevinclelland@gmail.com>
2024-02-02 15:47:10 +0200

    Update page styling and headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container w-1/2 h-screen mx-auto text-center bg-fixed bg-center" style="background-image: url('/wwjmd.png')">
+<div class="container max-w-4xl h-screen mx-auto text-center bg-fixed bg-center" style="background-image: url('/wwjmd.png')">
 	
 		
-		<h1 class="h1 p-4 text-slate-800">Welcome to The Greeters Guild!</h1>
+		<h1 class="text-5xl md:text-5xl font-bold p-4 text-slate-800">Welcome to The Greeters Guild</h1>
 
 		
-		<div class="h2 p-4 italic text-center mx-auto ">What would Jan Molby do?</div>
+		<div class="h2 p-4 font-bold italic text-center mx-auto ">What would Jan Molby do?</div>
 	
 </div>
